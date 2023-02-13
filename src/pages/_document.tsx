716924e0509f4cb6a5bd6441720a3115a6adbba8
FURUKAWA Akira <50062855+minguu42@users.noreply.github.com>
2023-02-13 18:03:43 +0900

    開発環境の構築 (#1)
    * fix: パッケージを更新する
    
    * fix!: tsconfig.jsonを修正する
    
    * fix: サンプルコードを修正する
    
    * fix: コードを整形する
    
    * feat: ptettierパッケージのインストール
    
    * fix: tsconfig.jsonを修正する
    
    * fix: importパスを修正する
    
    * feat: .prettierrc.jsonを追加する
    
    * fix: fmtスクリプトを追加する
    
    * fix: コードを整形する
    
    * fix: package.jsonを修正する
    
    * feat: ESLintに関する設定を追加する
    
    * feat: StyleLintに関する設定を追加する
    
    * feat: StyleLintに関するスクリプトを追加する
    
    * fix: コードを整形する
    
    * feat: CIワークフローを追加する
    
    * fix: 挨拶を修正する
    
    * fix: 依存関係を再構築する

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
-export default function Document() {
+const Document: React.FC = () => {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
