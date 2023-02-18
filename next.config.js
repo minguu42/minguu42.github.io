3e6863cf2374fa634508117ac5beaa84c5aa2e9f
minguu42 <minguu42@gmail.com>
2023-02-18 16:01:42 +0900

    feat: next-export-optimize-imagesのプラグインを追加する

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withExportImages = require("next-export-optimize-images");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withExportImages(nextConfig);
